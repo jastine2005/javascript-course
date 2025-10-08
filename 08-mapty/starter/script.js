1b84768c9e2486849c3551660c9dffa14b95e335
jastine2005 <jhaydhensalazar@gmail.com>
2025-10-08 13:35:00 +0800

    feat(mapty): integrate geolocation and interactive map features with Leaflet.js and App class setup

--- 08-mapty/starter/script.js
+++ 08-mapty/starter/script.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 console.log('=== MAPTY: WORKOUT TRACKING APPLICATION ===');
@@ -17,64 +16,64 @@ This hour focuses on the OOP foundation - the classes that will power our app!
 
 // Base Workout class
 class Workout {
-	date = new Date();
-	id = (Date.now() + '').slice(-10);
-	clicks = 0;
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+    clicks = 0;
 
-	constructor(coords, distance, duration) {
-		this.coords = coords; // [lat, lng]
-		this.distance = distance; // in km
-		this.duration = duration; // in min
-	}
+    constructor(coords, distance, duration) {
+        this.coords = coords; // [lat, lng]
+        this.distance = distance; // in km
+        this.duration = duration; // in min
+    }
 
-	_setDescription() {
-		// prettier-ignore
-		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    _setDescription() {
+        // prettier-ignore
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-		this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
-			months[this.date.getMonth()]
-		} ${this.date.getDate()}`;
-	}
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+            months[this.date.getMonth()]
+        } ${this.date.getDate()}`;
+    }
 
-	click() {
-		this.clicks++;
-	}
+    click() {
+        this.clicks++;
+    }
 }
 
 // Running subclass
 class Running extends Workout {
-	type = 'running';
+    type = 'running';
 
-	constructor(coords, distance, duration, cadence) {
-		super(coords, distance, duration);
-		this.cadence = cadence;
-		this.calcPace();
-		this._setDescription();
-	}
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+        this._setDescription();
+    }
 
-	calcPace() {
-		// min/km
-		this.pace = this.duration / this.distance;
-		return this.pace;
-	}
+    calcPace() {
+        // min/km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
 }
 
 // Cycling subclass
 class Cycling extends Workout {
-	type = 'cycling';
+    type = 'cycling';
 
-	constructor(coords, distance, duration, elevationGain) {
-		super(coords, distance, duration);
-		this.elevationGain = elevationGain;
-		this.calcSpeed();
-		this._setDescription();
-	}
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+        this._setDescription();
+    }
 
-	calcSpeed() {
-		// km/h
-		this.speed = this.distance / (this.duration / 60);
-		return this.speed;
-	}
+    calcSpeed() {
+        // km/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
 }
 
 // Test the Workout class
